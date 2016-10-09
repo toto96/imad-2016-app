de8dc0d22aa1f02768e6aad840a9df4636a12198
toto96 <debanjanroy96@gmail.com>
2016-10-09 09:05:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/WhatsApp Image 2016-08-10 at 8.37.39 PM.jpeg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'WhatsApp Image 2016-08-10 at 8.37.39 PM.jpeg'));
 });
 
